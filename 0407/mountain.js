3d2f420107be90404ae689c0c6d2e0141b9ced85
songhyejin <davics@naver.com>
2021-04-07 14:04:18 +0900

    mountain

---
+++ 0407/mountain.js
@@ -0,0 +1,27 @@
+const main = () => {
+    let count = 0;
+    while (count < 10) {
+        var maxH = 0;
+        var maxIndex = 0;
+    
+        for (let i = 0; i < 8; i++) {
+            const mountainH = parseInt(readline());
+            console.debug("Height of "+i+" mountain------------->>", mountainH);
+    
+            if(mountainH > maxH) {
+                maxH = mountainH;
+                maxIndex = i;
+            }
+        }
+
+        console.debug("max index ::", maxIndex);
+        count = count + 1;
+    }
+}
+
+const readline = () => {
+	const rand = Math.floor(Math.random() * 10);
+    return rand;
+}
+
+main();
